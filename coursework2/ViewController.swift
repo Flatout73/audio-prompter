6fbedb8066444e1fad2e3fb66488e05c193f75fd
Flatout73 <fantom2259@gmail.com>
2017-03-15 02:02:37 +0300

    Initial Commit

---
+++ coursework2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coursework2
+//
+//  Created by Леонид Лядвейкин on 15.03.17.
+//  Copyright © 2017 HSE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
